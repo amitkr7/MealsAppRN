19bc6ec599f8bf57e758b086b2d2e6fc84166a67
amitkr7 <amit840901@gmail.com>
2023-08-29 22:08:55 +0530

    Navigating Between two screens

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -1,6 +1,6 @@
 import { Pressable, Text, View, StyleSheet, Platform } from 'react-native';
 
-const CategoryGridTile = ({ title, color }) => {
+const CategoryGridTile = ({ title, color, onPress }) => {
   return (
     <View style={styles.gridItem}>
       <Pressable
@@ -8,7 +8,8 @@ const CategoryGridTile = ({ title, color }) => {
         style={({ pressed }) => [
           styles.button,
           pressed ? styles.buttonPressed : null,
-        ]}>
+        ]}
+        onPress={onPress}>
         <View style={[styles.innerContainer, { backgroundColor: color }]}>
           <Text style={styles.title}>{title}</Text>
         </View>
--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -2,13 +2,19 @@ import { FlatList } from 'react-native';
 import { CATEGORIES } from '../data/dummy-data';
 import CategoryGridTile from '../components/CategoryGridTile';
 
-const renderCategoryItem = (itemData) => {
-  return (
-    <CategoryGridTile title={itemData.item.title} color={itemData.item.color} />
-  );
-};
-
-const CategoriesScreen = () => {
+const CategoriesScreen = ({ navigation }) => {
+  const renderCategoryItem = (itemData) => {
+    const pressHandler = () => {
+      navigation.navigate('MealsOverview');
+    };
+    return (
+      <CategoryGridTile
+        title={itemData.item.title}
+        color={itemData.item.color}
+        onPress={pressHandler}
+      />
+    );
+  };
   return (
     <FlatList
       data={CATEGORIES}
