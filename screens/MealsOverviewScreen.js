eb2d10859aaeb9f97c2eca5733e60f48ee592a4b
amitkr7 <amit840901@gmail.com>
2023-08-30 21:19:56 +0530

    Displaying MealItem

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,13 +1,26 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
 
 import { MEALS } from '../data/dummy-data';
+import MealItem from '../components/MealItem';
 
 const MealsOverviewScreen = ({ route }) => {
   const catId = route.params.categoryId;
 
+  const displayedMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(catId) >= 0;
+  });
+
+  const renderMealItem = (itemData) => {
+    return <MealItem title={itemData.item.title} />;
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Meal Overview Screen-{catId}</Text>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      />
     </View>
   );
 };
