808989ed404fa989f1e17eda5cafb031fb41d858
amitkr7 <amit840901@gmail.com>
2023-09-06 21:32:45 +0530

    Bottom Tab and App finishing touches

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { Button, StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import { Ionicons } from '@expo/vector-icons';
 
 import CategoriesScreen from './screens/CategoriesScreen';
 import MealsOverviewScreen from './screens/MealsOverviewScreen';
@@ -19,13 +20,31 @@ const DrawerNavigator = () => {
         headerStyle: { backgroundColor: '#5d3004' },
         headerTintColor: 'white',
         sceneContainerStyle: { backgroundColor: '#5d3004' },
+        drawerContentStyle: { backgroundColor: '#5d3004' },
+        drawerInactiveTintColor: '#ffffff',
+        drawerActiveTintColor: '#5d3004',
+        drawerActiveBackgroundColor: '#eeba87',
       }}>
       <Drawer.Screen
         name='Categories'
         component={CategoriesScreen}
-        options={{ title: 'All Categories' }}
+        options={{
+          title: 'All Categories',
+          drawerIcon: ({ color, size }) => (
+            <Ionicons name='list' color={color} size={size} />
+          ),
+        }}
       />
-      <Drawer.Screen name='Favourites' component={FavouriteScreen} />
+      <Drawer.Screen
+        name='Favourites'
+        component={FavouriteScreen}
+        options={{
+          title: 'Favourites',
+          drawerIcon: ({ color, size }) => (
+            <Ionicons name='star' color={color} size={size} />
+          ),
+        }}
+      />
     </Drawer.Navigator>
   );
 };
